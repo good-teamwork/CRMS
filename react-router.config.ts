e0e3f5262fb767062fbc7d52ccb10012758c2f00
Eros-Aphrodite <lucky.super.star0805@gmail.com>
2025-10-21 16:31:22 +0900

    fix deploy

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,7 @@ export default {
 	appDirectory: './src/app',
 	ssr: true,
 	prerender: ['/*?'],
+	serverBuildFile: 'index.js',
+	serverPlatform: 'node',
+	serverModuleFormat: 'esm',
 } satisfies Config;
